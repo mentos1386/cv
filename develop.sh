d955cd869068b4dcacdc7a8db61d09ad6b0e0a6f
Tine Jozelj <tine@tjo.space>
2024-01-19 22:13:50 +0100

    feat: colors

--- develop.sh
+++ develop.sh
@@ -3,8 +3,8 @@ export JEKYLL_VERSION=3.8
 
 docker run --rm \
   --volume="$PWD:/srv/jekyll" \
-  --env JEKYLL_UID="1001" \
-  --env JEKYLL_GID="1001" \
+  --env JEKYLL_UID="$(id -u)" \
+  --env JEKYLL_GID="$(id -g)" \
   -p 4000:4000 \
   -it jekyll/jekyll:$JEKYLL_VERSION \
   jekyll serve --watch 
